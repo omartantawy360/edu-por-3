7d1b5b25b18fd83001ddb22f2ff2a42b8534561a
omartantawy <omartantawy360@gmail.com>
2026-02-05 21:58:30 +0200

    change

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/edu-por-2/',
+  base: '/edu-por-3/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
